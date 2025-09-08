2e308d91c8fcd3a1ec64043579a8b0b87217b3b1
hyuramua <sean61202@gmail.com>
2025-09-08 15:41:24 -0400

    Update and rename sketch.js to classnotes.js

--- classnotes.js
+++ classnotes.js
@@ -8,36 +8,36 @@ this is a mutlti line comment
 function setup() {
   //you must include set up function to run code.
   //only include ONE SET up function per sketch.
-  createCanvas(1000, 1000);
+  createCanvas(700, 700);
   //sets size of canvas area with width and height.
 
   //background(200); black and white background from 0-255
   background(255, 184, 210); //RGB color
 
   //drawing a line 
-  line(1000, 500, 1000, 500);
+  line(0, 0, 100, 100);
   //line function needs a starting point and ending point
   //line (x1, y1, x2, y2)
 // draw a line from points (500, 50) to (10, 300)
-line(900, 100, 800, 500); //x1,y1,x2,y2
+line(500, 50, 10, 300); //x1,y1,x2,y2
 
 //changing the stroke color (r, g, b)
 stroke(255,255, 0);
 strokeWeight(20);
 //triangle (x1, y1, x2, y2, x3, y3)
-triangle(0, 0, 0, 0, 0, 0);
+triangle(500, 300, 600, 600, 200, 400);
 
 // draw a rectangle 
-rect(0, 0, 0, 0);
+rect(100, 400, 400, 200);
 
 //changing the fill color
 fill(100, 200, 50);
 
 // drawing a quadrilateral 
-quad(0, 0, 0, 0, 0, 0, 0, 0);
+quad(100, 100, 200, 100, 300, 300, 300, 400);
 
 // drawing a ellipse
-ellipse(0, 0, 0, 0);
+ellipse(400, 300, 200, 200);
 // ellipse (x, y, w, h)
 
 }
