19ccfe3eb57e9bfa923a46210adf56b43da1a9a1
hyuramua <sean61202@gmail.com>
2025-11-13 21:06:59 -0500

    Delete SS2_ShaniaN/SS2_ShaniaN directory

--- SS2_ShaniaN/SS2_ShaniaN/sketch.js
+++
@@ -1,4 +0,0 @@
-// references
-
-// patt vira interactive clock cursor affect https://youtu.be/R4GHtY3fckk?si=aRgFNuMsYCey0PgP
-// patt vira's getting started with creative coding https://youtu.be/x1NxkEjfNtI?si=NTD7mrANfRUaLssI
