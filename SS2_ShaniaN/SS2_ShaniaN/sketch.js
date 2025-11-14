5efbfece0b307c249322dfe6a73588552a3ef8f7
hyuramua <sean61202@gmail.com>
2025-11-13 21:01:26 -0500

    SS2_ShaniaN
    notes and references included

---
+++ SS2_ShaniaN/SS2_ShaniaN/sketch.js
@@ -0,0 +1,4 @@
+// references
+
+// patt vira interactive clock cursor affect https://youtu.be/R4GHtY3fckk?si=aRgFNuMsYCey0PgP
+// patt vira's getting started with creative coding https://youtu.be/x1NxkEjfNtI?si=NTD7mrANfRUaLssI
